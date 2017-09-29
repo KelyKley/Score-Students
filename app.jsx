2a8bf4bef632f3eff919697fb165ea2c01692c86
KelyKley <kelyanamurohuamani@gmail.com>
2017-09-29 10:38:09 -0500

    Proyecto evaluado

--- app.jsx
+++ app.jsx
@@ -2,76 +2,99 @@ let players = [
   {
     name: "Jim Hoskins",
     score: 31,
-    id: 1,
+    id: 1
   },
   {
     name: "Andree Hoskins",
     score: 35,
-    id: 2,
+    id: 2
   },
   {
     name: "Alena Hoskins",
     score: 42,
-    id: 3,
-  },
+    id: 3
+  }
 ];
 
 const Header = () => {
   return (
     <div className="header">
-      <table>
-        <tr>
-          <td>PLAYERS:</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <td>TOTAL POINTS:</td>
-          <td>108</td>
-        </tr>
+      <table className="stats counter counter-player">
+        <tbody>
+          <tr>
+            <td>PLAYERS:</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>TOTAL POINTS:</td>
+            <td>108</td>
+          </tr>
+        </tbody>
       </table>
-      <div>
+      <div className="stopwatch">
         <h2>STOPWATH</h2>
         <h1 className="stopwatch-time">0</h1>
         <button>START</button>
         <button>RESET</button>
       </div>
-    </div>);
-}
+    </div>
+  );
+};
 
 const PlayerList = ({ players }) => {
   return (
     <div>
-      {players.map((item, index) => {
-        return (
-          <div className="player  ">
-            <div className="player-name counter-score">{item.name}</div>
-            <div className="player-score  ">
-              <div className="decrement counter-action"><strong> - </strong></div>
-              <div className="counter-score">{item.score}</div>
-              <div className="increment counter-action "> <strong>+</strong> </div>
-            </div>
-          </div>);
-      })
-      }
+      <div className="player counter">
+        <div className="player-name counter-score">{players[0].name}</div>
+        <div className="player-score counter">
+          <button className="player decrement counter-action"> - </button>
+          <div className="counter-score">{players[0].score}</div>
+          <button className="player increment counter-action "> + </button>
+        </div>
+      </div>
+
+      <div className="player counter">
+        <div className="player-name counter-score">{players[1].name}</div>
+        <div className="player-score counter">
+          <button className="player decrement counter-action"> - </button>
+          <div className="counter-score">{players[1].score}</div>
+          <button className="player increment counter-action "> + </button>
+        </div>
+      </div>
+
+      <div className="player counter">
+        <div className="player-name counter-score">{players[2].name}</div>
+        <div className="player-score counter">
+          <button className="player decrement counter-action"> - </button>
+          <div className="counter-score">{players[2].score}</div>
+          <button className="player increment counter-action "> + </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 const PlayerForm = () => {
   return (
-    <div className="">
-      <input type="text" placeholder="ENTER A NAME" />
-      <input type="submit" value="ADD PLAYER" />
-    </div>);
-}
+    <div className="add-player-form">
+      <form action="">
+        <input type="text" placeholder="ENTER A NAME" />
+        <input type="submit" value="ADD PLAYER" />
+      </form>
+    </div>
+  );
+};
 
 const Application = ({ title, players }) => {
   return (
-    <div>
+    <div className="scoreboard">
       <Header players={players} />
       <PlayerList players={players} />
       <PlayerForm />
     </div>
   );
-}
+};
 
-ReactDOM.render(<Application title="Scoreboard" players={players} />, document.getElementById('container'));
+ReactDOM.render(
+  <Application title="Scoreboard" players={players} />,
+  document.getElementById("container")
+);
